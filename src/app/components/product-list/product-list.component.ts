0f6d6fc64a59e55aa0b689f93436a4665a605923
Ruslan Temirkhanov <rusl0424@gmail.com>
2019-12-12 00:17:49 -0500

    Display the data in HTML and update Angular component.

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ProductService } from "src/app/services/product.service";
+import { Product } from "src/app/common/product";
 
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  selector: "app-product-list",
+  templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.css"]
 })
 export class ProductListComponent implements OnInit {
+  products: Product[];
 
-  constructor() { }
+  // Injecting ProductService
+  constructor(private productService: ProductService) {}
 
   ngOnInit() {
+    this.listProducts();
   }
 
+  listProducts() {
+    // Method is invoked once you "subscribe"
+    this.productService.getProductList().subscribe(data => {
+      this.products = data; // Assign results to Product Array
+    });
+  }
 }
