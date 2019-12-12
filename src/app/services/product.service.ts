82310712a0ecfe98a35f984665ed821e54f2e3f5
Ruslan Temirkhanov <rusl0424@gmail.com>
2019-12-11 23:48:25 -0500

    Created Product class and Product Service.

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Product } from "../common/product";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class ProductService {
+  // URL for Spring Boot REST API
+  private baseUrl = "http://localhost:8080/api/products";
 
-  constructor() { }
+  // Injecting httpClient
+  constructor(private httpClient: HttpClient) {}
+
+  // Map the JSON data from Spring Data REST to Product array
+  getProductList(): Observable<Product[]> {
+    return this.httpClient
+      .get<GetResponse>(this.baseUrl)
+      .pipe(map(response => response._embedded.products));
+  }
+}
+
+// Supporting interface. Helps with the mapping. Unwrappes the JSON data from Sring Rest API
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  };
 }
