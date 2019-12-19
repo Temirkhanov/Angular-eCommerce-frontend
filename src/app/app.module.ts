9f384d36b202c0854054d89f04b91cf45b70291a
Ruslan Temirkhanov <rusl0424@gmail.com>
2019-12-18 21:07:05 -0500

    Upgraded product-list template with new design. Added components for nav, cart and product-page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { AppComponent } from "./app.component";
 import { ProductListComponent } from "./components/product-list/product-list.component";
 import { HttpClientModule } from "@angular/common/http";
 import { ProductService } from "./services/product.service";
+import { NavComponent } from './components/nav/nav.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ProductPageComponent } from './components/product-page/product-page.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent],
+  declarations: [AppComponent, ProductListComponent, NavComponent, CartComponent, ProductPageComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [ProductService],
   bootstrap: [AppComponent]
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,27 +1,8 @@
 import { Component, OnInit } from "@angular/core";
-import { ProductService } from "src/app/services/product.service";
-import { Product } from "src/app/common/product";
 import { Category } from "src/app/common/category";
 import { CategoryService } from "src/app/services/category.service";
-
-// @Component({
-//   selector: "app-product-list",
-//   templateUrl: "./product-list.component.html",
-//   // templateUrl: "./product-list.component.html",
-//   styleUrls: ["./product-list.component.css"]
-// })
-// export class ProductListComponent implements OnInit {
-//   products: Product[];
-
-//   // Injecting ProductService
-//   constructor(private _productService: ProductService) {}
-
-//   ngOnInit() {
-//     this._productService
-//       .getProductList()
-//       .subscribe(data => (this.products = data)); // Get data by subscribing
-//   }
-// }
+import { ProductService } from "src/app/services/product.service";
+import { Product } from "src/app/common/product";
 
 @Component({
   selector: "app-product-list",
@@ -30,14 +11,14 @@ import { CategoryService } from "src/app/services/category.service";
   styleUrls: ["./product-list.component.css"]
 })
 export class ProductListComponent implements OnInit {
-  categories: Category[];
+  products: Product[];
 
   // Injecting ProductService
-  constructor(private _productService: CategoryService) {}
+  constructor(private _productService: ProductService) {}
 
   ngOnInit() {
     this._productService
-      .getCategoryList()
-      .subscribe(data => (this.categories = data)); // Get data by subscribing
+      .getProductList()
+      .subscribe(data => (this.products = data)); // Get data by subscribing
   }
 }
