f5532acfa3b8a2f003ce4229f7deab13a8a427b1
Ruslan Temirkhanov <rusl0424@gmail.com>
2020-01-13 00:38:50 -0500

    Generated new components for user: login, register, profile and for admin: dashboard, product list, user list.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,15 @@ import { AppRoutingModule, routingComponents } from "./app-routing.module";
 import { HomePageComponent } from "./components/home-page/home-page.component";
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CartCalComponent } from './components/cart-cal/cart-cal.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegisterComponent } from './components/user/register/register.component';
+import { ProfileComponent } from './components/user/profile/profile.component';
+import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
+import { UserListComponent } from './components/admin/user-list/user-list.component';
+import { ProductListComponent } from './components/admin/product-list/product-list.component';
 
 @NgModule({
-  declarations: [AppComponent, routingComponents, NavbarComponent, CartCalComponent],
+  declarations: [AppComponent, routingComponents, NavbarComponent, CartCalComponent, LoginComponent, RegisterComponent, ProfileComponent, DashboardComponent, UserListComponent, ProductListComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [ProductService],
   bootstrap: [AppComponent]
