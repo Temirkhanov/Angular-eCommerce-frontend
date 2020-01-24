995c44729d3112e9fbd76dd1a9cf0b0399480433
Ruslan Temirkhanov <rusl0424@gmail.com>
2020-01-24 13:01:16 -0500

    Updated Navbar with login, logout, register, profile and dashboard.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from "@angular/core";
+import { UserService } from "../../services/user.service";
+import { User } from "../../model/user";
+import { Role } from "../../model/role";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-navbar",
@@ -6,7 +10,23 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./navbar.component.css"]
 })
 export class NavbarComponent implements OnInit {
-  constructor() {}
+  currentUser: User;
+
+  constructor(private userService: UserService, private router: Router) {
+    this.userService.currentUser.subscribe(data => {
+      this.currentUser = data;
+    });
+  }
 
   ngOnInit() {}
+
+  logOut() {
+    this.userService.logOut().subscribe(data => {
+      this.router.navigate(["/login"]);
+    });
+  }
+
+  get isAdmin() {
+    return this.currentUser && this.currentUser.role === Role.ADMIN;
+  }
 }
