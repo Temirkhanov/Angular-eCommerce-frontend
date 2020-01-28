b058108e21499a2b17d5b34c69c43918693743ea
Ruslan Temirkhanov <rusl0424@gmail.com>
2020-01-27 21:54:27 -0500

    Created Admin service boilerplate.

--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { User } from "../model/user";
+import { Product } from "../model/product";
+import { Transaction } from "../model/transaction";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AdminService {
+  currentUser: User;
+  headers: HttpHeaders;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.currentUser = JSON.parse(localStorage.getItem("currentUser"));
+    this.headers = new HttpHeaders({
+      authorization: "Bearer " + this.currentUser.token,
+      "Content-Type": "application/json; charset=UTF-8"
+    });
+  }
 }
